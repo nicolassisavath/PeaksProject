72ea2a42b90d45ff5820fecfc7680aaa24e2e5c7
nisisdell <nisisdell@gmail.com>
2019-04-20 10:44:21 +0200

    DB, User entity + User repository

---
+++ api/marvel/src/Entity/User.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $login;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $favoritesNumber;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getFavoritesNumber(): ?int
+    {
+        return $this->favoritesNumber;
+    }
+
+    public function setFavoritesNumber(?int $favoritesNumber): self
+    {
+        $this->favoritesNumber = $favoritesNumber;
+
+        return $this;
+    }
+}
