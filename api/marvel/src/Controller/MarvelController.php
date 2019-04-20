884f496a8d8de92a0c1e655edeadf4b418b3194a
nisisdell <nisisdell@gmail.com>
2019-04-20 10:29:04 +0200

    php curl marvel

--- api/marvel/src/Controller/MarvelController.php
+++ api/marvel/src/Controller/MarvelController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class MarvelController extends AbstractController
 {
@@ -26,9 +27,52 @@ class MarvelController extends AbstractController
     {
 		return new JsonResponse([
             [
-                'title' => 'The Princess Bride',
+                'title' => 'The Princess Brides',
                 'count' => 0
             ]
         ]);
+    }
+
+     /**
+     * @Route("/test", name="test")
+     */
+    public function test()
+    {
+    	$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=5f9fafa4c65f4c31bc15b9301203835f&hash=c2376eedba988967c6a18a5f9bc4d41a&offset=100&limit=20",
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "GET",
+		  CURLOPT_POSTFIELDS => "",
+		  CURLOPT_HTTPHEADER => array(
+		    "Content-Type: application/json",
+		    //"Postman-Token: b3e60971-24b9-4265-9f1b-6e1e7ca2d728",
+		    "cache-control: no-cache"
+		  ),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		  echo "cURL Error #:" . $err;
+		} else {
+		  return new Response($response);
+		  //return new JsonResponse([$response]);
+		}
+
+		// return new JsonResponse([
+  //           [
+  //               'title' => 'The Princess plouf',
+  //               'count' => 0
+  //           ]
+  //       ]);
     }
 }
